9673053871400a8b2afd7ba5719b3ec536913884
robin.buehler <robin.buehler@aliunid.com>
2022-06-05 14:43:27 +0200

    update

--- open_pv_sim.py
+++ open_pv_sim.py
@@ -2,12 +2,13 @@ import dash
 from dash import dcc, html, Input, Output
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
+
 import plotly.graph_objs as go
+import plotly.express as px
 import pandas as pd
 import numpy as np
 import requests
 import datetime
-
 
 # Modules for PV panels
 import pvlib
@@ -98,14 +99,14 @@ def get_sonnendach(building_id):
     df.columns=['building_ids','feature_ids','PV_klasse','ausrichtung','Stromertrag','neigung','area']
     return df
 
-def pv_sim(lat,lon,roof_info):
+def pv_sim(roof_info):
     usefull_roof_area = 1
     PR = 0.85
 
-    weather = pvlib.iotools.get_pvgis_tmy(lat,lon,map_variables=True)[0]
+    weather = pvlib.iotools.get_pvgis_tmy(loc_info['lat'],loc_info['lon'],map_variables=True)[0]
     mod_lib = pvlib.pvsystem.retrieve_sam('cecmod')
     modules = mod_lib['Jinko_Solar__Co___Ltd_JKM385M_72L'] # average module with efficiency of 20%
-    location = pvlib.location.Location(latitude=lat, longitude=lon)
+    location = pvlib.location.Location(latitude=loc_info['lat'], longitude=loc_info['lon'])
     cec_inverters = pvlib.pvsystem.retrieve_sam('cecinverter')
     cec_inverter = cec_inverters['ABB__MICRO_0_25_I_OUTD_US_208__208V_'] # not really needed (since DC output is used later)
     temperature_model_parameters = TEMPERATURE_MODEL_PARAMETERS['sapm']['open_rack_glass_glass']
@@ -128,8 +129,40 @@ def pv_sim(lat,lon,roof_info):
 
     return ac_power
 
+def get_monthly_fig(df_monthly):
+    
+    fig = px.bar(df_monthly,labels={'value':'kWh','Power_kW':'kWh'})
+
+    return fig
+
+def get_daily_fig(df_daily):
+    fig = px.bar(df_daily,labels={'value':'kWh','Power_kW':'kWh'})
 
 
+    return fig
+
+def get_himmelsrichtung(roof_info):
+    degree_roof=roof_info.ausrichtung.values+180
+    direction = []
+    for d in degree_roof:
+        if d>310:
+            direction.append("Nord-West")
+        elif d>260:
+            direction.append("West")
+        elif d>220:
+            direction.append("Süd-West")
+        elif d>170:
+            direction.append("Süd")
+        elif d>130:
+            direction.append("Süd-Ost")
+        elif d>80:
+            direction.append("Ost")
+        elif d>40:
+            direction.append("Nord-Ost")
+        else:
+            direction.append("Nord")
+
+    return direction
 
 
 if __name__=="__main__":
@@ -139,6 +172,8 @@ if __name__=="__main__":
     app = dash.Dash(__name__)
     feature_options=['Dach1','Dach2']
     roof_info={}
+    loc_info={}
+    store_results={}
 
     app.layout = html.Div(
         children=[
@@ -163,8 +198,31 @@ if __name__=="__main__":
 
         
         html.Div(id='output_div'),
-        html.Div(id='output_pv')
+        html.Div(id='output_pv'),
 
+        html.Div(
+            children=[
+                html.H5(id='sub_title_M',children='Monatlicher Ertrag',),
+                dcc.Graph(id='monthly_fig')
+            ]
+        ),
+
+        html.Div(
+            children=[
+                html.H5(id='sub_title_D',children='Täglicher Ertrag',),
+                dcc.Graph(id='daily_fig')
+            ]
+        ),
+        
+        html.Div(
+            html.H2(id='max_P'),
+        ),
+
+        html.Div([
+            html.Button("Download PV-Leistung über 1 Jahr in stündlicher Auflösung.xlsx", id="save-button"),
+            dcc.Download(id="download-dataframe-xlsx"),
+            ]
+        )
         #html.Iframe(src="https://map.geo.admin.ch/?lang=de&topic=energie&bgLayer=ch.swisstopo.swissimage&catalogNodes=2419,2420,2427,2480,2429,2431,2434,2436,2767,2441,3206&layers=ch.bfe.solarenergie-eignung-daecher&zoom=22&X=234885&Y=675658&layers_opacity=0.65",
         #        style={"height": "1067px", "width": "100%"})
 
@@ -187,30 +245,69 @@ if __name__=="__main__":
             building_id = get_building_id(coord)
             df = get_sonnendach(building_id)
             feature_options = list(df.feature_ids)
-            
+            directions = get_himmelsrichtung(df)
             # store roof info in dictionary
-            
+            loc_info['lat']=lat
+            loc_info['lon']=lon
+
             for i in range(0,len(df)):
                 roof_info[df.iloc[i].feature_ids]=df.iloc[i]
 
-            return [],feature_options
+            dict_out = {feature_options[i]:directions[i] for i in range(len(directions))}
+            return [],dict_out
         
         else:
             return [],[]
 
-    @app.callback(Output('output_pv', 'children'),
-                Input('submit-button-2', 'n_clicks'),
-                Input('feature_id','value')
+    @app.callback([
+        Output('output_pv', 'children'),
+        Output('monthly_fig','figure'),
+        Output('daily_fig','figure'),
+        Output('max_P','children')],
+        [Input('submit-button-2', 'n_clicks'),
+        Input('feature_id','value')]
                   )
     def selected_roof(clicks,feature_id):
-        
+        res_daily=pd.DataFrame(365*[0])
+        res_monthly=pd.DataFrame(12*[0])
+        Pmax=0
         if clicks is not None:
             print(feature_id)
             for id in feature_id:
-                selected_roof = roof_info[id]
-                print(selected_roof)
-        
+                print(roof_info)
+                selected_roof = roof_info[float(id)]
+                ac_power = pv_sim(selected_roof)
+                store_results[id]=ac_power
 
+                res_daily = res_daily.add(ac_power.resample('D').sum().values)
+                res_monthly = res_monthly.add(ac_power.resample('M').sum().values)
+
+                fig_monthly = get_monthly_fig(res_monthly)
+                fig_daily = get_daily_fig(res_daily)
+                Pmax = Pmax+round(ac_power.max().values[0],1)
+                print(res_daily)
+            
+            return ac_power.sum(),fig_monthly,fig_daily,"Maximale Spitzenlast ist : {} kW".format(Pmax),
+        else:
+            PreventUpdate
+        
+    @app.callback(
+        Output("download-dataframe-xlsx", "data"),
+        Input("save-button", "n_clicks"),
+        Input("feature_id","value"),
+        prevent_initial_call=True)
+    def download_as_excel(n_clicks,feature_id):
+        if not n_clicks:
+            raise PreventUpdate
+        else:
+            
+            writer = pd.ExcelWriter('new_excel_file.xlsx', engine="xlsxwriter")
+            for id in feature_id:
+                print(store_results[id])
+                store_results[id].to_excel(writer, sheet_name=id)
+            writer.save()
+
+            return dcc.send_file('new_excel_file.xlsx')
 
 
     app.run_server(debug=True)
